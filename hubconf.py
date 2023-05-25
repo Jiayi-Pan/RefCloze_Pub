652cffbc20d3bee311fe065ad0561a52e6949064
Jiayi Pan <i@jiayipan.me>
2023-05-26 00:21:34 +0800

    u

--- hubconf.py
+++ hubconf.py
@@ -1,25 +1,14 @@
-# Copyright (c) Aishwarya Kamath & Nicolas Carion. Licensed under the Apache License 2.0. All Rights Reserved
 import torch
-
-from models.backbone import Backbone, Joiner, TimmBackbone
-from models.mdetr import MDETR
-from models.position_encoding import PositionEmbeddingSine
-from models.postprocessors import PostProcess, PostProcessSegm
-from models.segmentation import DETRsegm
-from models.transformer import Transformer
-
 from models import build_model
 
 dependencies = ["torch", "torchvision"]
 
-
 def base_model():
     """
-    Our base model with pre-trained Res50, RoBERTa-base, trained on Flickr-30k entities.
+    Our base model initialized from ResNet 50 and RoBERTa-base, pre-trained on Flickr-30k entities.
     """
-
     model_checkpoint = torch.hub.load_state_dict_from_url(
-        url="https://zenodo.org/record/4721981/files/pretrained_resnet101_checkpoint.pth",
+        url="https://github.com/Jiayi-Pan/temp/releases/download/Model/plain_model.pth"
         map_location="cpu",
         check_hash=True,
     )
