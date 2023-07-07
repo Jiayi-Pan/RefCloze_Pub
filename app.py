13bb6e7114bc77653789db9a98a435220073e3fc
Jiayi Pan <i@jiayipan.me>
2023-07-08 00:06:06 +0800

    finalize app.py

--- app.py
+++ app.py
@@ -3,21 +3,18 @@ import gradio as gr
 
 title = "OctoBERT"
 description = """Interactive Demo for OctoBERT. This base model is trained only on Flickr-30k."""
-# examples =[
-# ['swing.jpg','The woman stands outdoors, next to a child in a <mask>.'],
-# ['tennis.jpg', 'A woman in blue shorts and white shirt holds a tennis racket on a blue <mask> court.'],
-# ['birthday.jpg', 'The smiling <mask> is celebrating her <mask> party with friends, surrounded by balloons and a <mask> with candles.'],
-# ['skate.jpg', 'A person in a rainbow colored snowsuit is snowboarding down a <mask> slope.'],
-# ['street.jpg', 'A man with <mask> plays with a little girl while walking down the street, while an Asian woman walks ahead of them.'],
-# ['dog.jpg', 'A black dog stands on a <mask>, green fields behind him.'],
-# ]
+examples =[
+['https://i.imgur.com/ASXtqVc.jpg','The woman stands outdoors, next to a child in a <mask>.'],
+['https://i.imgur.com/UspUc7t.jpg', 'A woman in blue shorts and white shirt holds a tennis racket on a blue <mask> court.'],
+['https://i.imgur.com/IRUoMUg.jpg', 'The smiling <mask> is celebrating her <mask> party with friends, surrounded by balloons and a <mask> with candles.'],
+['https://i.imgur.com/23BZbaH.jpg', 'A person in a rainbow colored snowsuit is snowboarding down a <mask> slope.'],
+['https://i.imgur.com/hoQCg1h.jpg', 'A man with <mask> plays with a little girl while walking down the street, while an Asian woman walks ahead of them.'],
+['https://i.imgur.com/dFeN6bs.jpg', 'A black dog stands on a <mask>, green fields behind him.'],
+]
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, img_transform, tokenizer, post_processor, plot_results = torch.hub.load('Jiayi-Pan/RefCloze_Pub', 'flickr_base_model')
+# model, img_transform, tokenizer, post_processor, plot_results = torch.hub.load('.', 'flickr_base_model', source='local')
 model = model.to(device)
-
-# url = "https://i.imgur.com/G0rWulu.jpg"
-# img = PIL.Image.open(urlopen(url))
-# caption = "three<mask>in a room"
 
 def plot_inference(img, caption):
     imgs_tensor = img_transform(img).to(device).unsqueeze(0)
@@ -35,6 +32,6 @@ gr.Interface(
     [gr.outputs.Image(type="numpy", label="Output"), gr.outputs.Textbox(label="Predicted Words")],
     title=title,
     description=description,
-    # examples=examples,
-    cache_examples=False,
+    examples=examples,
+    cache_examples=True,
     ).launch()
